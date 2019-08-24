e5c9a9927c1427498439a95ab1f8cc10f593a57e
kai8wz <michal.kaliszewski@pl.bosch.com>
2019-08-24 16:33:54 +0200

    feat(init): spring init template setup

---
+++ src/test/java/com/recruit/githubrepositories/GithubRepositoriesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.recruit.githubrepositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubRepositoriesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

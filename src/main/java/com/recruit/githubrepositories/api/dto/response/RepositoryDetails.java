b7132ec54279b54dd853175809206c5c6aa81bd8
kai8wz <michal.kaliszewski@pl.bosch.com>
2019-09-01 20:17:24 +0200

    feat(dtos): setup dtos for github and api response; setup map struct

---
+++ src/main/java/com/recruit/githubrepositories/api/dto/response/RepositoryDetails.java
@@ -0,0 +1,36 @@
+package com.recruit.githubrepositories.api.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class RepositoryDetails implements Serializable {
+
+    private static final long serialVersionUID = 8269473897901856974L;
+
+    public static final int MAX_LENGTH_NAME = 35;
+
+    @NotEmpty
+    @Size(max = MAX_LENGTH_NAME)
+    private String fullName;
+
+    private String description;
+
+    @URL
+    private String cloneUrl;
+
+    @Min(value = 0)
+    private Integer stars;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate createdAt;
+}
---
+++ src/test/groovy/com/recruit/githubrepositories/converters/GitRepositoryMapperSpec.groovy
@@ -0,0 +1,40 @@
+
+
+import com.recruit.githubrepositories.api.dto.GithubRepositoryDetails
+import com.recruit.githubrepositories.api.dto.response.RepositoryDetails
+import com.recruit.githubrepositories.converters.GitRepositoryMapper
+import spock.lang.Specification
+
+import java.time.LocalDate
+
+class GitRepositoryMapperSpec extends Specification {
+
+
+    private GitRepositoryMapper gitRepositoryMapper
+
+
+    def setup() {
+        gitRepositoryMapper = GitRepositoryMapper.INSTANCE
+    }
+
+    def "Git repository mapper should map fields from GithubRepositoryDetails to RepositoryDetails"() {
+        given:
+            def githubRepositoryDetails = GithubRepositoryDetails.builder()
+                    .full_name("test name")
+                    .description("test description")
+                    .clone_url("https://test/clone")
+                    .created_at(LocalDate.now())
+                    .stargazers_count(0)
+                    .build()
+
+        when:
+            RepositoryDetails repositoryDetails = gitRepositoryMapper.githubRepositoryDetailsToRepositoryDetails(githubRepositoryDetails)
+
+        then:
+            repositoryDetails.fullName == "test name"
+            repositoryDetails.description == "test description"
+            repositoryDetails.cloneUrl == "https://test/clone"
+            repositoryDetails.createdAt == LocalDate.now()
+            repositoryDetails.stars == 0
+    }
+}

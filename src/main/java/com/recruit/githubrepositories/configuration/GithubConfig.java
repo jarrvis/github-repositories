bacf2c556816cb6f74c06b9ec5f8b33593671d5c
kai8wz <michal.kaliszewski@pl.bosch.com>
2019-09-01 18:20:44 +0200

    chore(config): init configuration of github api and swagger docs

---
+++ src/main/java/com/recruit/githubrepositories/configuration/GithubConfig.java
@@ -0,0 +1,20 @@
+package com.recruit.githubrepositories.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@ConfigurationProperties(prefix = "api.github")
+public class GithubConfig {
+    /**
+     * Configuration lass to encapsulate all config values to setup the REST interface to the Github API.
+     */
+
+    @NotEmpty
+    private String url;
+
+    @NotEmpty
+    private String cacheName;
+}
---
+++ src/main/java/com/recruit/githubrepositories/configuration/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.recruit.githubrepositories.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+/**
+ * Main configuration class to enable the Swagger UI frontend. It registers all supported controllers and describes the
+ * API.
+ * 
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "github repositories REST API",
+                "Consumes github API. Uses caching to save up requests rate.",
+                "1.0.0-dev",
+"",
+                new Contact("Michal Kaliszewski", "https://github.com/jarrvis", "michal.kaliszewski00@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+}

4a2ab8ff6705d9a6fdff07a4d5ad61744d35faae
kai8wz <michal.kaliszewski@pl.bosch.com>
2019-09-01 22:41:08 +0200

    feat(cache-http-response): setup http client with caching

--- src/main/java/com/recruit/githubrepositories/GithubRepositoriesApplication.java
+++ src/main/java/com/recruit/githubrepositories/GithubRepositoriesApplication.java
@@ -2,8 +2,10 @@ package com.recruit.githubrepositories;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GithubRepositoriesApplication {
 
 	public static void main(String[] args) {

58deee74961235376733c4236e1eb1599bab5f51
kai8wz <michal.kaliszewski@pl.bosch.com>
2019-09-09 14:37:32 +0200

    chore(java-doc): fix java doc formatting; remove qualifier

--- src/main/java/com/recruit/githubrepositories/configuration/GithubConfig.java
+++ src/main/java/com/recruit/githubrepositories/configuration/GithubConfig.java
@@ -5,12 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.validation.constraints.NotEmpty;
 
+/**
+ * Configuration class to encapsulate all config values to setup the REST interface to the Github API.
+ */
 @Data
 @ConfigurationProperties(prefix = "api.github")
 public class GithubConfig {
-    /**
-     * Configuration lass to encapsulate all config values to setup the REST interface to the Github API.
-     */
 
     @NotEmpty
     private String url;

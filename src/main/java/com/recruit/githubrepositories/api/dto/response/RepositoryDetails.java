7e74961e3772ed26a12cdb077ccd54d28cfc9dfe
kai8wz <michal.kaliszewski@pl.bosch.com>
2019-09-12 00:39:53 +0200

    chore(validation): remove not used jsr-303 validation annotation

--- src/main/java/com/recruit/githubrepositories/api/dto/response/RepositoryDetails.java
+++ src/main/java/com/recruit/githubrepositories/api/dto/response/RepositoryDetails.java
@@ -4,12 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.hibernate.validator.constraints.URL;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -21,20 +16,10 @@ public class RepositoryDetails implements Serializable {
 
     private static final long serialVersionUID = 8269473897901856974L;
 
-    public static final int MAX_LENGTH_NAME = 100;
 
-    @NotEmpty
-    @Size(max = MAX_LENGTH_NAME)
     private String fullName;
-
     private String description;
-
-    @URL
     private String cloneUrl;
-
-    @Min(value = 0)
     private Integer stars;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate createdAt;
 }

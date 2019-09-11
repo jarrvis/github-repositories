b1f7a5224b6bcf5ef94df08bed7fce3448d9fabf
kai8wz <michal.kaliszewski@pl.bosch.com>
2019-09-11 22:47:14 +0200

    test(serialize-deserialize): add dto serialization and deserialization tests

--- src/main/java/com/recruit/githubrepositories/api/dto/response/RepositoryDetails.java
+++ src/main/java/com/recruit/githubrepositories/api/dto/response/RepositoryDetails.java
@@ -1,6 +1,7 @@
 package com.recruit.githubrepositories.api.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RepositoryDetails implements Serializable {
